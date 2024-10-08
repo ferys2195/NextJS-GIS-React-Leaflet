b507ac11a22007297ae2db241ed11e35c2319e03
ferys2195 <fery21irawan.me@gmail.com>
2024-10-08 09:24:00 +0700

    hide zoom control

--- src/components/MapApp.tsx
+++ src/components/MapApp.tsx
@@ -1,11 +1,25 @@
 // components/Map.js
-import { MapContainer, Polygon, TileLayer, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  Polygon,
+  TileLayer,
+  useMapEvents,
+  ZoomControl,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import MapLayerControl from "./MapLayerControl";
 import React, { useState } from "react";
 import MaskingOutbox from "./MaskingOutbox";
 import OpacityControl from "./OpacityControl";
 import MapLegend from "./MapLegend";
+import MenuDrawer from "./MenuDrawer/MenuDrawer";
+
+const POSITION_CLASSES = {
+  bottomleft: "leaflet-bottom leaflet-left",
+  bottomright: "leaflet-bottom leaflet-right",
+  topleft: "leaflet-top leaflet-left",
+  topright: "leaflet-top leaflet-right",
+};
 
 const MapApp = () => {
   const [opacity, setOpacity] = useState<number>(0.5);
@@ -26,18 +40,17 @@ const MapApp = () => {
         center={[-2.2556399, 112.8491973]}
         zoom={zoomLevel}
         minZoom={13}
+        zoomControl={false}
         style={{ height: "100vh", width: "100%" }}
       >
+        <ZoomControl position="bottomright" />
         <MapZoomListener />
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="Map data © OpenStreetMap contributors"
-        />
         <MapLayerControl opacity={opacity} setOpacity={setOpacity} />
         <MaskingOutbox opacity={opacity} />
         <MapLegend />
       </MapContainer>
       <OpacityControl opacity={opacity} setOpacity={setOpacity} />
+      <MenuDrawer />
     </>
   );
 };
